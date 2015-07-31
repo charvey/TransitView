7e79c4d6a9196afc1a0fc4930e6efa06d583cb26
Chris Harvey <chrischarvey@outlook.com>
2015-07-30 20:09:49 -0400

    Initial commit of Solution

---
+++ src/TransitView.Data/TransitViewContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Entity;
+
+namespace TransitView.Data
+{
+    public class TransitViewContext:DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=Database.db");
+        }
+    }
+}
